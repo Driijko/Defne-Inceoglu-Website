b77413d10149de10c1e038787abf95c4e7ddd9de
Driijko <driijko@gmail.com>
2019-11-07 12:19:22 -0500

    Add text to painting and photo pages

--- script.js
+++ script.js
@@ -59,18 +59,28 @@ const zoomTextBox = document.querySelector(".zoomTextBox");
 let zoom = false;
 let currentZoomImage;
 
-
 // Each image has a different accompanying text, displayed in the zoomTextBox Element.
 // We'll organize these into three arrays, one for either the design, painting or photography pages.
-// We check to see if we are on the design, painting, or photography page.
-const currentNavString = document.querySelector(".currentNav").innerHTML;
+// We add an event listener that checks to see when a page has finished loading, and then checks
+// if we are on the design, painting or photo page.
+// Further down, on the event listener for clicking an image, we will use the variable currentPage
+// to determine which text to select to accompany a given image.
 let currentPage;
-if (currentNavString === "VISUAL DESIGN") currentPage = "design";
-else if (currentNavString === "PAINTING") currentPage = "painting";
-else if (currentNavString === "PHOTOGRAPHY") currentPage = "photography";
+let currentNavButton;
+window.addEventListener("load", event => {
+  currentNavButton = document.querySelector(".currentNav");
+  if (currentNavButton.innerHTML === "VISUAL DESIGN") {
+    currentPage = "design";
+  } else if (currentNavButton.innerHTML === "PAINTING") {
+    currentPage = "painting";
+  } else if (currentNavButton.innerHTML === "PHOTOGRAPHY") {
+    currentPage = "photography";
+  }
+});
 
 const designPageZoomTexts = [];
-designPageZoomTexts[0] = "Gentle Persuasion EP Cover, cassette tape and zine (2013)";
+designPageZoomTexts[0] =
+  "Gentle Persuasion EP Cover, cassette tape and zine (2013)";
 designPageZoomTexts[1] = "Fallen Love Records Logo (2012-2013)";
 designPageZoomTexts[2] = "Fallen Love Records Poster (2013)";
 designPageZoomTexts[3] = "Arkadas Logo (2013)";
@@ -86,17 +96,86 @@ designPageZoomTexts[12] = "Rise Above Ad #2 (2017)";
 designPageZoomTexts[13] = "Vintage Tech Poster (2018)";
 designPageZoomTexts[14] = "Form on a Pastel Plane: Feel (2017)";
 designPageZoomTexts[15] = "Form on a Pastel Plane: Hashtag (2017)";
-designPageZoomTexts[16] = "Form on a Pastel Plane: (2017)";
-designPageZoomTexts[17] = "Form on a Pastel Plane: (2017)";
-designPageZoomTexts[18] = "Form on a Pastel Plane: (2017)";
+designPageZoomTexts[16] = "Form on a Pastel Plane: Hexa-gone (2017)";
+designPageZoomTexts[17] =
+  "Form on a Pastel Plane: I Miraculously Woke Up (2017)";
+designPageZoomTexts[18] =
+  "Form on a Pastel Plane: Moons Waxing and Waning (2017)";
 designPageZoomTexts[19] = "Form on a Pastel Plane: Push Here to Cry (2017)";
 designPageZoomTexts[20] = "Form on a Pastel Plane: Pyramids at Night (2017)";
-designPageZoomTexts[21] = "Form on a Pastel Plane: Vampire Teeth Falling (2017)";
+designPageZoomTexts[21] =
+  "Form on a Pastel Plane: Vampire Teeth Falling (2017)";
 
 const paintingPageZoomTexts = [];
-
+paintingPageZoomTexts[0] = "No Middle (2014) Pencil Crayon on Paper";
+paintingPageZoomTexts[1] = "Self-Portrait (2015) Watercolour on Paper";
+paintingPageZoomTexts[2] = "Mountain Man (2014) Pencil Crayon on Paper";
+paintingPageZoomTexts[3] = "Untitled (2014) Pencil Crayon on Paper";
+paintingPageZoomTexts[4] =
+  "Self-Portrait (2014) Acrylic and Watercolour on Paper";
+paintingPageZoomTexts[5] = "Bathtub 2 (2012) Watercolour on Paper";
+paintingPageZoomTexts[6] = "Sketchbook Page (2012) Pen on Paper";
+paintingPageZoomTexts[7] = "Kissin (2013) Watercolour and Pencil on Paper";
+paintingPageZoomTexts[8] =
+  "Self-Portrait (2013) Watercolour and Collage on Paper";
+paintingPageZoomTexts[9] = "Don't Bother (2012) Ink and Watercolour on Paper";
+paintingPageZoomTexts[10] =
+  "A Birthday Card (2014) Ink and Pencil-Crayon on Paper";
+paintingPageZoomTexts[11] = "Bathtub (2011) Acrylic and Watercolour on Paper";
+paintingPageZoomTexts[12] =
+  "Mountain Man Revisits (2015) Ink and Acrylic on Cardboard";
+paintingPageZoomTexts[13] =
+  "Self-Portrait (2014) Acrylic and Watercolour on Paper";
+paintingPageZoomTexts[14] = "Deathbed (2012) Watercolour and Collage on Paper";
+paintingPageZoomTexts[15] = "Black Hole Lover (2012) Ink on Paper";
+paintingPageZoomTexts[16] =
+  "Self-Portrait (2012) Acrylic and Watercolour on Paper";
+paintingPageZoomTexts[17] = "Andrew (2012) Acrylic and Watercolour on Paper";
+paintingPageZoomTexts[18] = "Lindsay (2014) Acrylic and Watercolour on Paper";
+paintingPageZoomTexts[19] =
+  "Afghan Center (2012) Acrylic, Watercolour and Collage on Paper";
+paintingPageZoomTexts[20] =
+  "Self-Portrait (2014) Acrylic and Watercolour on Paper";
+paintingPageZoomTexts[21] = "It's Really Foggy Today (2012) Ink on Paper";
+paintingPageZoomTexts[22] = "Green Guy (2013) Acrylic and Watercolour on Paper";
+paintingPageZoomTexts[23] = "Sketchbook Page (2014) Pencil-Crayon on Paper";
+paintingPageZoomTexts[24] =
+  "Self-Portrait (2014) Acrylic and Watercolour on Paper";
 
 const photoPageZoomTexts = [];
+photoPageZoomTexts[0] = "New York (2012)";
+photoPageZoomTexts[1] = "Paris (2012)";
+photoPageZoomTexts[2] = "Shelby (2013)";
+photoPageZoomTexts[3] = "Denis (2013)";
+photoPageZoomTexts[4] = "Versailles (2012)";
+photoPageZoomTexts[5] = "Munich (2012)";
+photoPageZoomTexts[6] = "That's Entertainment (2014)";
+photoPageZoomTexts[7] = "New York City (2012)";
+photoPageZoomTexts[8] = "Times Square (2013)";
+photoPageZoomTexts[9] = "Coney Island (2013)";
+photoPageZoomTexts[10] = "Chinatown (2013)";
+photoPageZoomTexts[11] = "Munich (2012)";
+photoPageZoomTexts[12] = "Vimy Ridge (2012)";
+photoPageZoomTexts[13] = "Defne (2014)";
+photoPageZoomTexts[14] = "Gord #1 (2016)";
+photoPageZoomTexts[15] = "Legs (2016)";
+photoPageZoomTexts[16] = "Gord #2 (2016)";
+photoPageZoomTexts[17] = "Gord #3 (2016)";
+photoPageZoomTexts[18] = "Open in Kelowna (2016)";
+photoPageZoomTexts[19] = "Budweiser (2012)";
+photoPageZoomTexts[20] = "Wall Street Rose (2013)";
+photoPageZoomTexts[21] = "Sandcastles at Coney Island (2013)";
+photoPageZoomTexts[22] = "Cart (2011)";
+photoPageZoomTexts[23] = "Brandon (2014)";
+photoPageZoomTexts[24] = "My Mom and Aunt in New York City (2013)";
+photoPageZoomTexts[25] = "My Kitchen Window (2013)";
+photoPageZoomTexts[26] = "Brian (2014)";
+photoPageZoomTexts[27] = "Pool (2015)";
+photoPageZoomTexts[28] = "Chris (2014)";
+photoPageZoomTexts[29] = "Carolina (2014)";
+photoPageZoomTexts[30] = "Paris (2012)";
+photoPageZoomTexts[31] = "Paris (2012)";
+photoPageZoomTexts[32] = "London (2012)";
 
 // Zoom in on an image by clicking it.
 const clickGridPic = function(event) {
@@ -108,11 +187,11 @@ const clickGridPic = function(event) {
         zoomImageList[i].style.display = "block";
         currentZoomImage = zoomImageList[i];
         zoom = true;
-        if (currentPage = "design") {
+        if (currentPage === "design") {
           zoomTextBox.innerHTML = designPageZoomTexts[i];
-        } else if (currentPage = "painting") {
+        } else if (currentPage === "painting") {
           zoomTextBox.innerHTML = paintingPageZoomTexts[i];
-        } else if (currentPage = "photography") {
+        } else if (currentPage === "photography") {
           zoomTextBox.innerHTML = photoPageZoomTexts[i];
         }
         break;
